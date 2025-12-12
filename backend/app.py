344fb43ea7e7f90b415f3ecae84434281e539ec8
Anusha Narang <147870940+narang-an@users.noreply.github.com>
2025-12-12 12:51:15 -0500

    Revert "Step 6"

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,10 @@
-from flask import Flask, request
-import os
+from flask import Flask
 
 app = Flask(__name__)
-
-app.config['UPLOAD_FOLDER'] = 'uploads'
 
 @app.route('/')
 def home():
     return "Hello, Flask!"
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    file = request.files.get('image')
-
-    if not file:
-        return {"success": False, "message": "No image uploaded"}
-
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-    file.save(file_path)
-
-    return {
-        "success": True,
-        "filename": file.filename,
-        "message": "Uploaded successfully"
-    }
 
 if __name__ == '__main__':
     app.run(debug=True)
